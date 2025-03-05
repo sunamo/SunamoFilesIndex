76823e09275be0affb1b735831f563a6cc6268f9
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:15 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- _sunamo/SH.cs
+++ _sunamo/SH.cs
@@ -1,9 +1,4 @@
 namespace SunamoFilesIndex._sunamo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class SH
 {
@@ -12,15 +7,12 @@ internal class SH
     {
         nazevPP = FirstCharUpper(nazevPP);
     }
-
-
     internal static string FirstCharUpper(string nazevPP)
     {
         if (nazevPP.Length == 1)
         {
             return nazevPP.ToUpper();
         }
-
         string sb = nazevPP.Substring(1);
         return nazevPP[0].ToString().ToUpper() + sb;
     }
