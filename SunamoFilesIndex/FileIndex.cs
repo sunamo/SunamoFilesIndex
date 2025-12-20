81a8f49e3c6b6cdbd554c09bbcc2d51a0fc24f16
Radek Jancik <radek.jancik@usys.cz>
2025-12-20 13:38:47 +0100

    Upgrade to net10.0

--- SunamoFilesIndex/FileIndex.cs
+++ SunamoFilesIndex/FileIndex.cs
@@ -1,22 +1,18 @@
 // EN: Variable names have been checked and replaced with self-descriptive names
 // CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoFilesIndex;
-
 /// <summary>
 /// Připomíná práci s databází - k označení složek se používají čísla int
 ///
 /// Working with CheckBoxData
 /// Use FolderItem, FileItem,
 /// </summary>
-public class FileIndex
+public partial class FileIndex
 {
-    #region Only single static variable relativeDirectories
     /// <summary>
     /// Without base paths
     /// </summary>
     static readonly List<string> relativeDirectories = [];
-    #endregion
-    #region Variables
     /// <summary>
     ///
     /// </summary>
@@ -33,36 +29,34 @@ public class FileIndex
     ///
     /// </summary>
     static readonly List<string> directories = [];
-    public string? BasePath
-    {
-        get;
-        private set;
-    }
-    #endregion
-    #region Instance method
+    public string? BasePath { get; private set; }
+
     /// <summary>
     /// Get folders with name A2. A1 is IDParent
     /// </summary>
-    /// <param name="prohledavatSlozky"></param>
-    /// <param name="name"></param>
+    /// <param name = "prohledavatSlozky"></param>
+    /// <param name = "name"></param>
     public IList<FolderItem> GetFoldersWithName(int[] prohledavatSlozky, string name)
     {
         if (prohledavatSlozky == null)
         {
             return _folders.Where(c => c.Name == name).ToList();
         }
+
         return _folders.Where(c => c.Name == name).Where(d => prohledavatSlozky.Contains(d.IDParent)).ToList();
     }
+
     public List<FileItem> FindAllFilesWithName(string name)
     {
         return files.FindAll(d => d.Name == name);
     }
+
     /// <summary>
     /// Process all files including subfolders
     ///
     /// A1 musí být cesta zakončená slashem
     /// </summary>
-    /// <param name="folder"></param>
+    /// <param name = "folder"></param>
     public void AddFolderRecursively(string folder)
     {
         folder = FS.WithEndSlash(folder);
@@ -75,8 +69,10 @@ public class FileIndex
             _folders.Add(GetFolderItem(item));
             AddFilesFromFolder(folder, item);
         }
+
         AddFilesFromFolder(folder, FS.WithoutEndSlash(folder));
     }
+
     /// <summary>
     /// Index all files from A3.
     ///
@@ -85,13 +81,14 @@ public class FileIndex
     /// A3 - full path to actual folder
     /// Add with relative file path
     /// </summary>
-    /// <param name="basePath"></param>
-    /// <param name="folder"></param>
+    /// <param name = "basePath"></param>
+    /// <param name = "folder"></param>
     private void AddFilesFromFolder(string basePath, string folder)
     {
         var files2 = Directory.GetFiles(folder, "*.*", SearchOption.TopDirectoryOnly);
         files2.ToList().ForEach(c => files.Add(GetFileItem(c, basePath)));
     }
+
     private FolderItem GetFolderItem(string p)
     {
         FolderItem fi = new()
@@ -102,27 +99,30 @@ public class FileIndex
         };
         return fi;
     }
+
     /// <summary>
     /// Return index of folder or -1 if cannot found
     /// </summary>
-    /// <param name="folder"></param>
+    /// <param name = "folder"></param>
     public int GetRelativeFolder(string folder)
     {
         folder = FS.WithEndSlash(folder);
         return relativeDirectories.IndexOf(folder);
     }
+
     public string GetRelativeFolder(int folder)
     {
         return relativeDirectories[folder];
     }
+
     /// <summary>
     /// Return object FIleItem.
     /// Add to relativeDirectories, if A3.
     ///
     /// A3 - whether save to relativeDirectories and can use indexes for directory
     /// </summary>
-    /// <param name="p"></param>
-    /// <param name="basePath"></param>
+    /// <param name = "p"></param>
+    /// <param name = "basePath"></param>
     private FileItem GetFileItem(string p, string basePath)
     {
         FileItem fi = new()
@@ -139,6 +139,7 @@ public class FileIndex
         {
             throw new Exception($"{basePathName} is null");
         }
+
         string relDirName = basePathName.Replace(basePath, "");
         if (!relativeDirectories.Contains(relDirName))
         {
@@ -150,9 +151,11 @@ public class FileIndex
         {
             fi.IDRelativeDirectory = relativeDirectories.IndexOf(relDirName) + 1;
         }
+
         //}
         return fi;
     }
+
     /// <summary>
     /// Clear folders and files collection
     /// </summary>
@@ -161,21 +164,22 @@ public class FileIndex
         _folders.Clear();
         files.Clear();
     }
+
     public int GetIndexOfFolder(FolderItem item)
     {
         return _folders.IndexOf(item);
     }
+
     public IList<FileItem> GetFilesInRelativeFolder(int p)
     {
         return files.Where(c => c.IDRelativeDirectory == p).ToList();
     }
-    #endregion
-    #region Has FileIndex as input or output parameter
+
     /// <summary>
     /// Process recursively A1 - for every folder one object FileIndex in output
     ///
     /// </summary>
-    /// <param name="folders"></param>
+    /// <param name = "folders"></param>
     public static Dictionary<string, FileIndex> IndexFolders(IList<string> folders)
     {
         Dictionary<string, FileIndex> vr = [];
@@ -185,8 +189,10 @@ public class FileIndex
             fi.AddFolderRecursively(item);
             vr.Add(item, fi);
         }
+
         return vr;
     }
+
     /// <summary>
     /// Prida do A3 soubor s relativni cestou pokud neexistuje
     /// Use relative path to file to find relative id directory and insert with file path to ID to A3
@@ -196,10 +202,10 @@ public class FileIndex
     /// A3 - key is relative file path, value is index of relative directory
     /// A4 - relative paths to files which is used to fill A3. no change
     /// </summary>
-    /// <param name="folderOfSolution"></param>
-    /// <param name="fi"></param>
-    /// <param name="relativeFilePathForEveryColumn"></param>
-    /// <param name="filesFromAllFoldersUniqueRelative"></param>
+    /// <param name = "folderOfSolution"></param>
+    /// <param name = "fi"></param>
+    /// <param name = "relativeFilePathForEveryColumn"></param>
+    /// <param name = "filesFromAllFoldersUniqueRelative"></param>
     public static void AggregateFilesFromAllFolders(string folderOfSolution, FileIndex fi, Dictionary<string, int> relativeFilePathForEveryColumn, List<string> filesFromAllFoldersUniqueRelative)
     {
         foreach (var item2 in fi.files)
@@ -212,6 +218,7 @@ public class FileIndex
             }
         }
     }
+
     /// <summary>
     /// Tato metoda má za úkol vytvořit matici ze souborů v A1, kde každý soubor bude v daném sloupci dle A2
     /// Kdyz soubor nebude existovat bude null
@@ -219,12 +226,12 @@ public class FileIndex
     /// Load size of files from disc
     /// In key of A2 - relativeFilePath, value - index of column.
     /// </summary>
-    /// <param name="files"></param>
-    /// <param name="relativeFilePathForEveryColumn"></param>
-    public static CheckBoxDataShared<TWithSize<string>?>[,] ExistsFilesOnDrive(Dictionary<string, FileIndex> files, Dictionary<string, int> relativeFilePathForEveryColumn)
+    /// <param name = "files"></param>
+    /// <param name = "relativeFilePathForEveryColumn"></param>
+    public static CheckBoxDataShared<TWithSize<string>?>[, ] ExistsFilesOnDrive(Dictionary<string, FileIndex> files, Dictionary<string, int> relativeFilePathForEveryColumn)
     {
         int columns = relativeFilePathForEveryColumn.Count;
-        CheckBoxDataShared<TWithSize<string>?>[,] vr = new CheckBoxDataShared<TWithSize<string>?>[files.Count, columns];
+        CheckBoxDataShared<TWithSize<string>?>[, ] vr = new CheckBoxDataShared<TWithSize<string>?>[files.Count, columns];
         int r = -1;
         // Process all rows
         foreach (var item in files)
@@ -244,7 +251,14 @@ public class FileIndex
                 {
                     long l2 = new FileInfo(fullFilePath).Length;
                     // To result set CheckBoxData - full path and size
-                    vr[r, columnToInsert] = new CheckBoxDataShared<TWithSize<string>?> { t = new TWithSize<string> { t = fullFilePath, size = l2 } };
+                    vr[r, columnToInsert] = new CheckBoxDataShared<TWithSize<string>?>
+                    {
+                        t = new TWithSize<string>
+                        {
+                            t = fullFilePath,
+                            size = l2
+                        }
+                    };
                 }
                 else
                 {
@@ -254,131 +268,7 @@ public class FileIndex
                 }
             }
         }
+
         return vr;
     }
-    #endregion
-    #region Not use FileIndex
-    // TODO: Not use FileIndex, move to other locations
-    /// <summary>
-    /// Check (or uncheck) all in columns by filesize
-    /// </summary>
-    /// <param name="allRows"></param>
-    public static CheckBoxDataShared<TWithSize<string>>[,] CheckVertically(CheckBoxDataShared<TWithSize<string>>[,] allRows)
-    {
-        int columns = allRows.GetLength(1);
-        int rows = allRows.GetLength(0);
-        // List all files
-        for (int c = 0; c < columns; c++)
-        {
-            // Create collections for all rows
-            // key - row, value - size
-            Dictionary<int, long> fileSize = [];
-            // For easy compare of size and find out any difference
-            List<long> fileSize2 = [];
-            for (int r = 0; r < rows; r++)
-            {
-                CheckBoxDataShared<TWithSize<string>> cbd = allRows[r, c];
-                if (cbd != null)
-                {
-                    if (cbd.t == null)
-                    {
-                        throw new Exception($"{cbd.t} is null");
-                    }
-                    fileSize.Add(r, cbd.t.size);
-                    fileSize2.Add(cbd.t.size);
-                }
-            }
-            #region Get min and max size
-            fileSize2.Sort();
-            long min = fileSize2[0];
-            long max = fileSize2[^1];
-            #endregion
-            #region Tick potencially unecesary files
-            if (fileSize.Count > 1)
-            {
-                if (min == max)
-                {
-                    TickIfItIsForDelete(allRows, 0, c, fileSize, min, max, false);
-                    for (int r = 1; r < rows; r++)
-                    {
-                        TickIfItIsForDelete(allRows, r, c, fileSize, min, max, true);
-                    }
-                }
-                else
-                {
-                    for (int r = 0; r < rows; r++)
-                    {
-                        TickIfItIsForDelete(allRows, r, c, fileSize, min, max, null);
-                    }
-                }
-            }
-            else
-            {
-                // Maybe leave file with zero size?
-                TickIfItIsForDelete(allRows, 0, c, fileSize, min, max, false);
-            }
-            #endregion
-        }
-        return allRows;
-    }
-    /// <summary>
-    /// Check CheckBox in condition of size and A7 in location specified parameter row A2 and column A3
-    ///
-    /// A4 to find size of file. In keys are indexes.
-    /// If size is A5 min, check.
-    /// If A6 max, uncheck.
-    /// Or none of this, set null. This behaviour can be changed setted A7 forceToAll
-    /// </summary>
-    /// <param name="allRows"></param>
-    /// <param name="row"></param>
-    /// <param name="column"></param>
-    /// <param name="fileSize"></param>
-    /// <param name="min"></param>
-    /// <param name="max"></param>
-    /// <param name="forceToAll"></param>
-    private static void TickIfItIsForDelete(CheckBoxDataShared<TWithSize<string>>[,] allRows, int row, int column, Dictionary<int, long> fileSize, long min, long max, bool? forceToAll)
-    {
-        CheckBoxDataShared<TWithSize<string>> cbd = allRows[row, column];
-        if (cbd != null)
-        {
-            long filSiz = fileSize[row];
-            if (filSiz == -1)
-            {
-            }
-            else if (filSiz == max)
-            {
-                if (forceToAll.HasValue)
-                {
-                    cbd.tick = forceToAll.Value;
-                }
-                else
-                {
-                    cbd.tick = false;
-                }
-            }
-            else if (filSiz == min)
-            {
-                if (forceToAll.HasValue)
-                {
-                    cbd.tick = forceToAll.Value;
-                }
-                else
-                {
-                    cbd.tick = true;
-                }
-            }
-            else
-            {
-                if (forceToAll.HasValue)
-                {
-                    cbd.tick = forceToAll.Value;
-                }
-                else
-                {
-                    cbd.tick = null;
-                }
-            }
-        }
-    }
-    #endregion
 }
---
+++ SunamoFilesIndex/FileIndex1.cs
@@ -0,0 +1,138 @@
+// EN: Variable names have been checked and replaced with self-descriptive names
+// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
+namespace SunamoFilesIndex;
+/// <summary>
+/// Připomíná práci s databází - k označení složek se používají čísla int
+///
+/// Working with CheckBoxData
+/// Use FolderItem, FileItem,
+/// </summary>
+public partial class FileIndex
+{
+    // TODO: Not use FileIndex, move to other locations
+    /// <summary>
+    /// Check (or uncheck) all in columns by filesize
+    /// </summary>
+    /// <param name = "allRows"></param>
+    public static CheckBoxDataShared<TWithSize<string>>[, ] CheckVertically(CheckBoxDataShared<TWithSize<string>>[, ] allRows)
+    {
+        int columns = allRows.GetLength(1);
+        int rows = allRows.GetLength(0);
+        // List all files
+        for (int c = 0; c < columns; c++)
+        {
+            // Create collections for all rows
+            // key - row, value - size
+            Dictionary<int, long> fileSize = [];
+            // For easy compare of size and find out any difference
+            List<long> fileSize2 = [];
+            for (int r = 0; r < rows; r++)
+            {
+                CheckBoxDataShared<TWithSize<string>> cbd = allRows[r, c];
+                if (cbd != null)
+                {
+                    if (cbd.t == null)
+                    {
+                        throw new Exception($"{cbd.t} is null");
+                    }
+
+                    fileSize.Add(r, cbd.t.size);
+                    fileSize2.Add(cbd.t.size);
+                }
+            }
+
+#region Get min and max size
+            fileSize2.Sort();
+            long min = fileSize2[0];
+            long max = fileSize2[^1];
+#endregion
+#region Tick potencially unecesary files
+            if (fileSize.Count > 1)
+            {
+                if (min == max)
+                {
+                    TickIfItIsForDelete(allRows, 0, c, fileSize, min, max, false);
+                    for (int r = 1; r < rows; r++)
+                    {
+                        TickIfItIsForDelete(allRows, r, c, fileSize, min, max, true);
+                    }
+                }
+                else
+                {
+                    for (int r = 0; r < rows; r++)
+                    {
+                        TickIfItIsForDelete(allRows, r, c, fileSize, min, max, null);
+                    }
+                }
+            }
+            else
+            {
+                // Maybe leave file with zero size?
+                TickIfItIsForDelete(allRows, 0, c, fileSize, min, max, false);
+            }
+#endregion
+        }
+
+        return allRows;
+    }
+
+    /// <summary>
+    /// Check CheckBox in condition of size and A7 in location specified parameter row A2 and column A3
+    ///
+    /// A4 to find size of file. In keys are indexes.
+    /// If size is A5 min, check.
+    /// If A6 max, uncheck.
+    /// Or none of this, set null. This behaviour can be changed setted A7 forceToAll
+    /// </summary>
+    /// <param name = "allRows"></param>
+    /// <param name = "row"></param>
+    /// <param name = "column"></param>
+    /// <param name = "fileSize"></param>
+    /// <param name = "min"></param>
+    /// <param name = "max"></param>
+    /// <param name = "forceToAll"></param>
+    private static void TickIfItIsForDelete(CheckBoxDataShared<TWithSize<string>>[, ] allRows, int row, int column, Dictionary<int, long> fileSize, long min, long max, bool? forceToAll)
+    {
+        CheckBoxDataShared<TWithSize<string>> cbd = allRows[row, column];
+        if (cbd != null)
+        {
+            long filSiz = fileSize[row];
+            if (filSiz == -1)
+            {
+            }
+            else if (filSiz == max)
+            {
+                if (forceToAll.HasValue)
+                {
+                    cbd.tick = forceToAll.Value;
+                }
+                else
+                {
+                    cbd.tick = false;
+                }
+            }
+            else if (filSiz == min)
+            {
+                if (forceToAll.HasValue)
+                {
+                    cbd.tick = forceToAll.Value;
+                }
+                else
+                {
+                    cbd.tick = true;
+                }
+            }
+            else
+            {
+                if (forceToAll.HasValue)
+                {
+                    cbd.tick = forceToAll.Value;
+                }
+                else
+                {
+                    cbd.tick = null;
+                }
+            }
+        }
+    }
+}
